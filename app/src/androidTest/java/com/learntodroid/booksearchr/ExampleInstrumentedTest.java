c16aa301aa17bbb2eea32a6cad1861c35007f500
sayanbyjuite <sayan.kar@byjus.com>
2022-08-31 23:53:01 +0530

    renamed packagename

--- app/src/androidTest/java/com/learntodroid/booksearchr/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/learntodroid/booksearchr/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.learntodroid.mvvmrestapi", appContext.getPackageName());
+        assertEquals("com.learntodroid.booksearchr", appContext.getPackageName());
     }
 }
